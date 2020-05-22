b74bcdd8dfa49fa3db11e6a6a07c3827b5a36f03
Daniel Merys <dkmerys@gmail.com>
2020-05-22 13:26:20 -0700

    add multiple views and routes

--- app.rb
+++ app.rb
@@ -1,6 +1,54 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pg')
+require('pry')
+require('./lib/project')
+require('./lib/volunteer')
 also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker"})
+
+get('/') do
+  # home page
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:album)
+end
+
+post('/projects') do
+  title = params[:title]
+  project = Project.new({:title => title, :id => nil})
+  project.save
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/:id/edit') do
+ @project = Project.find(params[:id].to_i())
+ erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:title])
+  @projects = Project.all
+  erb(:projects)
+end
+
+delete('projects/:id') do
+  project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
