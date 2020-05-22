6cdf9994e72572bf12dc7305bfb30cf0a6fa96d2
Daniel Merys <dkmerys@gmail.com>
2020-05-22 09:19:33 -0700

    pass returns all volunteers test

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -10,4 +10,22 @@ class Volunteer
   def ==(volunteer_to_compare)
     self.name == volunteer_to_compare.name
   end
+  
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers")
+    volunteers = []
+    returned_volunteers.each do |volunteer| 
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      project_id = volunteer.fetch("project_id").to_i
+      volunteers.push(Volunteer.new({:name => name, :id => id, :project_id => project_id}))
+    end
+    volunteers
+  end
+
+  def save
+    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
 end
