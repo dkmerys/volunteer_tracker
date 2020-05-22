b1429c7bfaf45e35a4407b176c5d2e496e12092d
Daniel Merys <dkmerys@gmail.com>
2020-05-22 11:16:56 -0700

    pass delete test, but not update

--- lib/project.rb
+++ lib/project.rb
@@ -37,4 +37,14 @@ class Project
     Volunteer.find_by_project(self.id)
   end
 
+  # def update(attributes)
+  #   @title = attributes[:title]
+  #   @id = attributes[:id]
+  #   DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+  # end
+
+  def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+  end
+
 end
